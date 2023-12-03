80e2c8a97116d17042b503a5ec05dd8f1f85b48d
Nate Henty <nate.henty99@gmail.com>
2023-12-03 12:54:50 -0600

    added day one part 2

--- problem1/problem1_aoc.java
+++ problem1/problem1_aoc.java
@@ -4,40 +4,83 @@ import java.lang.*;
 public class problem1_aoc{
     public static void main(String[] args) throws Exception
     {
-        File file = new File("E:/Advent_of_Code/puzzle1.txt");
+        int problem1 = problem1("E:/Advent_of_Code/problem1/puzzle1.txt");
+        System.out.println(problem1);
+
+        // problem 2
+        int problem2 = problem1pt2("E:/Advent_of_Code/problem1/problem1pt2.txt");
+        System.out.println(problem2);
+    }
+
+    public static int problem1(String filename) throws Exception
+    {
+        File file = new File(filename);
         Scanner sc = new Scanner(file);
         
         int total = 0;
         while (sc.hasNextLine())
         {
             String line = sc.next();
-            int digits = 0;
-            for (int i = 0; i < line.length(); i++) {
-                if (line.charAt(i) >= 48 && line.charAt(i) <= 57)
+            String strip_line = line.replaceAll("\\D+","");
+            int line_int = Integer.parseInt(strip_line);
+            int first_digit = Integer.parseInt(Integer.toString(line_int).substring(0, 1));
+            int last_digit = lastDigit(line_int);
+            int line_total = 0;
+            String combined = Integer.toString(first_digit) + Integer.toString(last_digit);
+            line_total = Integer.parseInt(combined);
+
+            total += line_total;
+        }
+        return total;
+    }
+
+
+    public static int problem1pt2(String filename) throws Exception
+    {
+        File file = new File(filename);
+        Scanner sc = new Scanner(file);
+        
+        Dictionary<String, String> num_dict = new Hashtable<>();
+        num_dict.put("one","o1e");
+        num_dict.put("two","t2o");
+        num_dict.put("three","thr3e");
+        num_dict.put("four","f4ur");
+        num_dict.put("five","f5ve");
+        num_dict.put("six","s6x");
+        num_dict.put("seven","s7ven");
+        num_dict.put("eight","e8ght");
+        num_dict.put("nine","n9ne");
+
+        int total = 0;
+        while (sc.hasNextLine())
+        {
+            Enumeration<String> keys_str = num_dict.keys();
+            String line = sc.next();
+            while (keys_str.hasMoreElements())
+            {
+                String keyname = keys_str.nextElement();
+                if (line.contains(keyname))
                 {
-                    digits++;
+                    line = line.replace(keyname,num_dict.get(keyname));
+                    System.out.println(keyname);
+                    System.out.println(num_dict.get(keyname));
+                    System.out.println(line);
+
                 }
-                    
             }
             String strip_line = line.replaceAll("\\D+","");
             int line_int = Integer.parseInt(strip_line);
             int first_digit = Integer.parseInt(Integer.toString(line_int).substring(0, 1));
             int last_digit = lastDigit(line_int);
             int line_total = 0;
-            // if (digits < 2) 
-            // {
-            //     line_total = last_digit; 
-            // } else {
-            //     String combined = Integer.toString(first_digit) + Integer.toString(last_digit);
-            //     line_total = Integer.parseInt(combined);
-            // }
             String combined = Integer.toString(first_digit) + Integer.toString(last_digit);
             line_total = Integer.parseInt(combined);
 
             total += line_total;
         }
-        System.out.println(total);
+        return total;
     }
+
 
     public static int lastDigit(int integer_in)
     {
