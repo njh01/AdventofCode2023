cec40350ba164200f25278d56a1b223f2aaac571
Nate Henty <nate.henty99@gmail.com>
2023-12-06 00:12:57 -0600

    problem7 init

---
+++ problem7/problem7_aoc.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.File;
+import java.io.FileInputStream;
+
+public class problem7_aoc
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Classs for tomorrow");
+    }
+}
