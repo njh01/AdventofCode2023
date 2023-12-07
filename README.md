a2a0483a07032a1169f058c3ff4d8fc25f1931df
Nate Henty <nate.henty99@gmail.com>
2023-12-06 23:47:56 -0600

    tonight aint it

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## Advent of code 2023
 
 Code for advent of code problems.
+
+Other java repos:
+
+- https://github.com/linl33/adventofcode/tree/year2023/year2023/src/main/java/dev/linl33/adventofcode/year2023
+- https://github.com/Ivancm11/adventOfCode2023/tree/main/out/production/AdventOfCode2023
--- problem7/problem7_aoc.java
+++ problem7/problem7_aoc.java
@@ -5,8 +5,39 @@ import java.io.FileInputStream;
 
 public class problem7_aoc
 {
-    public static void main (String[] args)
+    public static void main (String[] args) throws Exception
     {
-        System.out.println("Classs for tomorrow");
+        String filename = "E:/Advent_of_Code/problem7/problem7.txt";
+        File file = new File(filename);
+        FileInputStream fis = new FileInputStream(file);
+        byte[] data = new byte[(int) file.length()];
+        fis.read(data);
+        fis.close();
+        Map<String, Integer> card_dict = new Hashtable<>();
+        card_dict.put("A",14);
+        card_dict.put("K",13);
+        card_dict.put("Q",12);
+        card_dict.put("J",11);
+        card_dict.put("T",10);
+        card_dict.put("9",9);
+        card_dict.put("8",8);
+        card_dict.put("7",7);
+        card_dict.put("6",6);
+        card_dict.put("5",5);
+        card_dict.put("4",4);
+        card_dict.put("3",3);
+        card_dict.put("2",2);
+
+        String str = new String(data, "UTF-8");
+        String[] hands_n_bids = str.split("\n");
+        String[] hands = new String[hands_n_bids.length];
+        Integer[] bids = new Integer[hands_n_bids.length];
+        for (int i = 0; i < hands_n_bids.length; i++)
+        {   
+            String[] line = hands_n_bids[i].split("\\s+");
+            hands[i] = line[0];
+            bids[i] = Integer.parseInt(line[1]);
+        }
+        
     }
 }
